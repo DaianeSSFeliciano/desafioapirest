d791b20766a2cfed900a26ed4a4172afa92509b8
DaianeSSFeliciano <daiane.feliciano@globalweb.com.br>
2021-11-29 13:35:42 -0300

    desafio]

---
+++ spring/src/main/java/Main/BeerstockApplication.java
@@ -0,0 +1,14 @@
+package Main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class BeerstockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerstockApplication.class, args);
+    }
+
+}
---
+++ spring/src/main/java/exception/BeerAlreadyRegisteredException.java
@@ -0,0 +1,8 @@
+package exception;
+
+    public class BeerAlreadyRegisteredException extends Exception{
+
+        public BeerAlreadyRegisteredException(String beerName) {
+            super(String.format("Beer with name %s already registered in the system.", beerName));
+        }
+}
